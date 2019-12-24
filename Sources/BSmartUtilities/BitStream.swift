c7b096af33f097535d24345c681a6c78a791916f
Sebastian Toivonen <sebastian.toivonen@gmail.com>
2019-12-24 11:02:08 +0200

    Addition

--- Sources/BSmartUtilities/BitStream.swift
+++ Sources/BSmartUtilities/BitStream.swift
@@ -15,7 +15,7 @@ enum BitStreamError: Error {
 struct FloatCompressor {
     var minValue: Float
     var maxValue: Float
-    var bits: Int
+    var bits: Int // How many bits we want
     private var maxBitValue: Double
 
     init(minValue: Float, maxValue: Float, bits: Int) {
